bf235acf26ad75f01ef265cba5880c093183c461
AndreySolomakhin <ancl-sam@yandex.ru>
2025-12-13 20:57:37 +0300

    Класс ChooseHandler Реализована цикличность работы программы и возможность выхода из него Реализован пользовательский интерфейс для получения и обработки вариантов загрузки информации в список

--- src/main/java/example/gradle/bigproject/Main.java
+++ src/main/java/example/gradle/bigproject/Main.java
@@ -1,7 +1,13 @@
 package example.gradle.bigproject;
 
+import example.gradle.bigproject.ui.ChooseHandler;
+
 public class Main {
     public static void main(String[] args){
+
+        while (ChooseHandler.isWork){
+            ChooseHandler.handleChoose(ChooseHandler.getChoose());
+        }
 
     }
 }
