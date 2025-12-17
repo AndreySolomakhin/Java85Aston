cb1c91c66e37d47df7edc4c337dd1ed55f54ea49
a1111 <malik200030@mail.ru>
2025-12-17 20:00:25 +0300

    Реализована сортировка чётных элементов

---
+++ src/main/java/sorting/collection/StudentSorter.java
@@ -0,0 +1,10 @@
+package sorting.collection;
+
+import example.gradle.bigproject.model.Student;
+
+import java.util.List;
+
+public interface StudentSorter {
+
+    void sort(List<Student> students);
+}
