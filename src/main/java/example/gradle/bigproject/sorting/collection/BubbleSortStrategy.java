3908cc4fd344a3e472a7a51fe81280d19d7582f3
Polina11111111 <kurbatova_apollinnariya@mail.ru>
2025-12-24 15:22:10 +0300

    Реализован алгоритм сортировки BubbleSort и интерфейс стратегий

---
+++ src/main/java/example/gradle/bigproject/sorting/collection/BubbleSortStrategy.java
@@ -0,0 +1,26 @@
+package example.gradle.bigproject.sorting.collection;
+
+
+import java.util.Comparator;
+
+public class BubbleSortStrategy<T> {
+
+    public void sort(CustomArrayList<T> list, Comparator<? super T> comparator) {
+        // Проверка на пустой список или 1 элемент
+        if (list == null || list.size() < 2) {
+            return;
+        }
+
+        for (int i = 0; i < list.size() - 1; i++) {
+            for (int j = 0; j < list.size() - i - 1; j++) {
+                // Твоя строка с проверкой
+                if (comparator.compare(list.get(j), list.get(j + 1)) > 0) {
+                    // Логика замены элементов через промежуточную переменную
+                    T temp = list.get(j);
+                    list.set(j, list.get(j + 1));
+                    list.set(j + 1, temp);
+                }
+            }
+        }
+    }
+}
