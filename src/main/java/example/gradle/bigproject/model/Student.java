60a381d26b3a6cd77e15f454f85b07260031d75f
Polina11111111 <kurbatova_apollinnariya@mail.ru>
2025-12-27 12:18:44 +0300

    Отредактировала

--- src/main/java/example/gradle/bigproject/model/Student.java
+++ src/main/java/example/gradle/bigproject/model/Student.java
@@ -8,12 +8,11 @@ public final class Student {
     public static CustomArrayList<Student> studentList = new CustomArrayList<>();
 
     private final String studentName;
-    private final double gpa;
+    private final int gpa;
     private final int recordBookNumber;
 
-    // Компараторы
     public static final Comparator<Student> BY_RECORD_BOOK = Comparator.comparingInt(Student::getRecordBookNumber);
-    public static final Comparator<Student> BY_GPA = Comparator.comparingDouble(Student::getGpa);
+    public static final Comparator<Student> BY_GPA = Comparator.comparingInt(Student::getGpa);
     public static final Comparator<Student> BY_STUDENT_NAME = Comparator.comparing(Student::getStudentName);
 
     private Student(StudentBuilder b) {
@@ -22,57 +21,37 @@ public final class Student {
         this.recordBookNumber = b.recordBookNumber;
     }
 
-    public static StudentBuilder builder() {
-        return new StudentBuilder();
-    }
+    public static StudentBuilder builder() { return new StudentBuilder(); }
 
     public String getStudentName() { return studentName; }
-    public double getGpa() { return gpa; }
+    public int getGpa() { return gpa; }
     public int getRecordBookNumber() { return recordBookNumber; }
 
     @Override
     public String toString() {
-        return String.format("Студент: %-15s | Зачетка: %-5d | Балл: %.1f", studentName, recordBookNumber, gpa);
+        return String.format("Студент: %-15s | Зачетка: %-5d | Балл: %d", studentName, recordBookNumber, gpa);
     }
 
-    // Необходимы для работы MultithreadedCounter (поиск элемента)
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Student student = (Student) o;
-        return Double.compare(student.gpa, gpa) == 0 &&
-                recordBookNumber == student.recordBookNumber &&
-                Objects.equals(studentName, studentName);
+        return gpa == student.gpa && recordBookNumber == student.recordBookNumber &&
+                Objects.equals(studentName, student.studentName);
     }
 
     @Override
-    public int hashCode() {
-        return Objects.hash(studentName, gpa, recordBookNumber);
-    }
+    public int hashCode() { return Objects.hash(studentName, gpa, recordBookNumber); }
 
     public static class StudentBuilder {
-        private double gpa;
+        private int gpa;
         private String studentName;
         private int recordBookNumber;
 
-        public StudentBuilder setGpa(double gpa) {
-            this.gpa = gpa;
-            return this;
-        }
-
-        public StudentBuilder setStudentName(String studentName) {
-            this.studentName = studentName;
-            return this;
-        }
-
-        public StudentBuilder setRecordBookNumber(int recordBookNumber) {
-            this.recordBookNumber = recordBookNumber;
-            return this;
-        }
-
-        public Student build() {
-            return new Student(this);
-        }
+        public StudentBuilder setGpa(int gpa) { this.gpa = gpa; return this; }
+        public StudentBuilder setStudentName(String studentName) { this.studentName = studentName; return this; }
+        public StudentBuilder setRecordBookNumber(int recordBookNumber) { this.recordBookNumber = recordBookNumber; return this; }
+        public Student build() { return new Student(this); }
     }
 }
