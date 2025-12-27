3a9c40b676d41dbbc47fc8e3fd1de886261e2620
Polina11111111 <kurbatova_apollinnariya@mail.ru>
2025-12-27 12:03:21 +0300

    Отредактировала

--- src/main/java/example/gradle/bigproject/handlers/strategy/FillRandomStrategy.java
+++ src/main/java/example/gradle/bigproject/handlers/strategy/FillRandomStrategy.java
@@ -1,38 +1,27 @@
 package example.gradle.bigproject.handlers.strategy;
 
-
 import example.gradle.bigproject.model.Student;
+import example.gradle.bigproject.validators.StudentValidator;
 import java.util.Random;
 import java.util.stream.Stream;
 
 public class FillRandomStrategy implements ResponseStrategy {
-
     @Override
     public void handleResponse() {
         Random rnd = new Random();
         String[] names = {"Алексей", "Мария", "Иван", "Елена", "Дмитрий", "Анна"};
+        System.out.println(" --- Генерация студентов ---");
 
-        System.out.println(" Генерируем случайных студентов...");
-
-        // Используем Stream API для генерации 5 студентов
-        Stream.generate(() -> {
-                    String randomName = names[rnd.nextInt(names.length)];
-                    int randomGpa = rnd.nextInt(101); // 0-100
-                    int randomRecord = 10000 + rnd.nextInt(90000); // Пятизначный номер
-
-
-                    return new Student.StudentBuilder()
-                            .setStudentName(randomName)
-                            .setGpa(randomGpa)
-                            .setRecordBookNumber(randomRecord)
-                            .build();
-                })
-                .limit(5) // Создаем ровно 5 объектов
-                .forEach(student -> {
-                    Student.studentList.add(student);
-                    System.out.println(" Сгенерирован: " + student);
+        Stream.generate(() -> Student.builder()
+                        .setStudentName(names[rnd.nextInt(names.length)])
+                        .setGpa(rnd.nextInt(101))
+                        .setRecordBookNumber(10000 + rnd.nextInt(90000))
+                        .build())
+                .filter(StudentValidator::validate)
+                .limit(5)
+                .forEach(s -> {
+                    Student.studentList.add(s);
+                    System.out.println(" Добавлен: " + s);
                 });
-
-        System.out.println(" Список успешно заполнен!");
     }
 }
